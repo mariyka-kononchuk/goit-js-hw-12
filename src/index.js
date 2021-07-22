341fd94781d7fa971e77775ed52e1e799fb28305
Maria Kononchuk <mariyka.kononchuk@gmail.com>
2021-07-22 19:28:46 +0300

    загрузила исходные данные

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './sass/main.scss';
+import "./js/fetchCountries"
+
+const DEBOUNCE_DELAY = 300;
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,3 @@
+
+
+//https://restcountries.eu/rest/v2/all?fields=name;capital;population;flag;languages
